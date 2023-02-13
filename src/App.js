4d697b12ceb7640ad750953acb05430b72255318
windson86 <petrovic1986@gmail.com>
2023-02-13 16:27:57 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
+import styled from "styled-components";
+
+function App() {
+  const [inputMath, setInput] = useState("");
+  const [result, updateResult] = useState({ math: "", error: "" });
+  const apiData = {
+    expr: inputMath,
+  };
+
+  const Button = styled.button`
+    background-color: black;
+    color: white;
+    font-size: 20px;
+    padding: 10px 60px;
+    border-radius: 5px;
+    margin: 10px 0px;
+    cursor: pointer;
+  `;
+
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  function handleMath() {
+    axios
+      .post("http://api.mathjs.org/v4/", apiData)
+      .then(function (response) {
+        console.log(response.data);
+        updateResult({ math: response.data.result, error: "" });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">Test my math skills</header>
+      <div className="App-main">
+        <div class="centered">
+          <input
+            type="text"
+            id="mathProblem"
+            name="mathProblem"
+            onChange={handleChange}
+            value={inputMath}
+          />
+        </div>
+        <div class="centered">
+          <Button onClick={handleMath}>TEST</Button>
+        </div>
+        <div class="centered">
+          {" "}
+          <text>result:{result.math}</text>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
